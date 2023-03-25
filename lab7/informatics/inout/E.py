4af88cde0a0f0717dc50b545f49c13ea8e694a92
Tomiris <98753027+tmsnn@users.noreply.github.com>
2023-03-26 02:30:32 +0600

    python informatics

---
+++ lab7/informatics/inout/E.py
@@ -0,0 +1,9 @@
+s = 109
+v = int(input())
+t = int(input())
+# if v > 0: 
+#     print(v*t-s)
+# elif v < 0:
+#     print(s+v*t)
+
+print((v * t) % s)
---
+++ lab7/informatics/loops/C.py
@@ -0,0 +1,5 @@
+a = int(input())
+b = int(input())
+for x in [i*i for i in range(b) if i*i >= a and i*i <= b]:
+    print(x)
+    
